26f8d8e2afe0180610aef967e70a7f87843d6ac9
Ty-Miki <ethioadvance@gmail.com>
2024-09-10 21:19:10 +0300

    Finished dashboard development

--- app/main.py
+++ app/main.py
@@ -4,16 +4,47 @@ import os, sys
 from pathlib import Path
 
 current_directory = Path(__file__).parent
-top_10_handset_path = str(current_directory) + '\\data\\top_10_handsets.pkl'
+top_10_handset_path = str(current_directory) + '\\dashboard_data\\top_10_handsets.pkl'
+avearge_engagement_metric_path = str(current_directory) + '\\dashboard_data\\average_engagement.pkl'
+top_10_satisfied_path = str(current_directory) + '\\dashboard_data\\top_10_satisfied.pkl'
 
 if __name__ == '__main__':
     app = StreamlitAPP()
+
+    # Deploy top 10 handsets
     top_10_handset = app.load_dataframe(picke_path=top_10_handset_path)
 
     app.display_dataframe(df=top_10_handset,
-                          title='Top 5 Handsets Used By Customers')
+                          title='Top 10 Handsets Used By Customers')
     app.plot_bar_chart(df=top_10_handset,
                        title='Bar Chart: Top 10 Handsets Used By Customers',
                        x_label='Handset Type',
                        y_label='Count')
     
+    # Deploy Average engagement metric
+    avearge_engagement_metric = app.load_dataframe(picke_path=avearge_engagement_metric_path)
+
+    app.display_dataframe(df=avearge_engagement_metric,
+                          title='Average engagement metric per cluster')
+    app.plot_bar_chart(df=avearge_engagement_metric,
+                       title='Bar Chart: Average engagement metric per cluster',
+                       x_label="Clusters",
+                       y_label="Average Value")
+    
+    # Deploy top 10 satisfied customers
+    top_10_satisfied_customers = app.load_dataframe(picke_path=top_10_satisfied_path)
+
+    app.display_dataframe(df=top_10_satisfied_customers,
+                          title='Top 10 Satisfied Customers')
+    
+    app.plot_melted_bar_chart(df=top_10_satisfied_customers,
+                              title="Bar Chart: Top 10 Satisfied Customers",
+                              x_label="MSISDN/Number",
+                              y_label="Score",
+                              id_vars='MSISDN/Number',
+                              value_vars=['Engagement Score', 'Experience Score', 'Satisfaction Score'],
+                              rotation=90)
+    
+    app.show_correlation_with_heatmap(df=top_10_satisfied_customers,
+                                      columns=['Engagement Score', 'Experience Score', 'Satisfaction Score'],
+                                      title='Heatmap of Correlations')
--- app/utils.py
+++ app/utils.py
@@ -2,6 +2,7 @@
 import streamlit as st
 import pandas as pd
 import matplotlib.pyplot as plt
+import seaborn as sns
 import os
 import logging
 
@@ -13,12 +14,9 @@ class StreamlitAPP:
     def __init__(self) -> None:
         self.parent_dir = os.getcwd()
         st.title("Telecom Data Analysis for TellCo.")
-        st.write("Analyzing ")
         logger.info("StreamlitApp instance initialized successfully")
 
     def load_dataframe(self, picke_path: str) -> pd.DataFrame:
-        # The picke path should be relatice to the project parent directory
-        # Ex: '/notebooks/user_satisfaction_analysis/satisfaction_data.pkl
 
         try:
             data = pd.read_pickle(picke_path)
@@ -33,7 +31,7 @@ class StreamlitAPP:
 
         try:
             st.write(title)
-            st.dataframe(df.head())
+            st.dataframe(df)
             logger.info("Successfully displayed dataframe into streamlit app.")
         except Exception as e:
             logger.error(f"Error displaying dataframe {df} to streamlit app. {str(e)}")
@@ -42,10 +40,67 @@ class StreamlitAPP:
                        df: pd.DataFrame,
                        title: str,
                        x_label: str,
-                       y_label: str) -> None:
-        # Plot a streamline barchart based on given parameters.
-        df.plot(kind='bar', figsize=(10, 6))
-        plt.title(title)
-        plt.xlabel(x_label)
-        plt.ylabel(y_label)
-        st.pyplot(plt)
+                       y_label: str,
+                       rotation: int = 0) -> None:
+        
+        try:
+            # Plot a streamline barchart based on given parameters.
+            df.plot(kind='bar', figsize=(10, 6))
+            plt.title(title)
+            plt.xticks(rotation=rotation)
+            plt.xlabel(x_label)
+            plt.ylabel(y_label)
+            st.pyplot(plt)
+            logger.info(f"Bar chart plotted successfully for {df}")
+        
+        except Exception as e:
+            logger.error(f"Error plotting bar chart for {df}. {str(e)}")
+
+    def plot_melted_bar_chart(self, 
+                              df: pd.DataFrame,
+                              title: str,
+                              x_label: str,
+                              y_label: str,
+                              id_vars: str,
+                              value_vars: list,
+                              rotation: int = 0) -> None:
+        
+        try:
+            # Melt the dataframe for bar plot
+            melted_df = df.melt(id_vars=id_vars, value_vars=value_vars)
+            logger.info(f'Dataframe {df} melted successfully.')
+
+            # plot the bar chart
+            fig, ax = plt.subplots(figsize=(10, 6))
+            sns.barplot(x=id_vars, y='value', hue='variable', data=melted_df, ax=ax)
+            plt.title(title)
+            plt.xticks(rotation=rotation)
+            plt.xlabel(x_label)
+            plt.ylabel(y_label)
+            st.pyplot(fig)
+            logger.info(f'Bar chart plotted successfully for {df}')
+        
+        except Exception as e:
+            logger.error(f"Error plotting bar chart for {df}. {str(e)}")
+
+    def show_correlation_with_heatmap(self,
+                                      df: pd.DataFrame,
+                                      columns: list,
+                                      title: str) -> None:
+        try:
+            st.write(title)
+        
+            # Calculate correlations
+            correlations = df[columns].corr()
+            logger.info(f'Successfully calculated correlation between {columns} {df}')
+
+            # Plot the heatmap
+            fig, ax = plt.subplots()
+            sns.heatmap(correlations, annot=True, cmap='coolwarm', ax=ax)
+            st.pyplot(fig)
+            logger.info(f"Correlation heatmap between {columns} plotted successfully for {df}")
+        
+        except Exception as e:
+            logger.error(f"Error plotting correlation heatmap between {columns} for {df}. {str(e)}")
+
+        
